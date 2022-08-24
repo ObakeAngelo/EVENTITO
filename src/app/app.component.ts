fdffdad3a67221c2159b9be69435337ca3d51c7f
Angelo Ramirez <onitrash708@gmail.com>
2022-08-23 22:07:00 -0400

    Evento que me cambia un texto al realizar un check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'proyecto';
+  textoCheck = 'no eri del colo';
+  checkRegistro = false;
+  setregistro() {
+    this.textoCheck = 'si eri del colo';
+  }
 }
