ee37e64160a56804ebe300ba5aa3e17cea628fb4
Angelo Ramirez <onitrash708@gmail.com>
2022-08-23 23:11:49 -0400

    Se realizo 2 eventos
    El primer evento es que al escribir en el input y darle al enter imprima todo lo que escribio y el segundo es que si acerco el mause a la imagen cambie a otra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto';
-  textoCheck = 'no eri del colo';
+  textoCheck: string = 'no eri del colo';
+  textoInput: string = '';
   checkRegistro = false;
   setregistro() {
     this.textoCheck = 'si eri del colo';
   }
+  ngOnInit(): void {}
+  onKeyUp(event: any) {
+    this.textoInput += event.target.value + ' | ';
+  }
+  imgUpNeutral =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNNjFSZofLQtLYQxgSbjLz9RAVHuHmjkxsKdvjZwRr&s';
+  imgUpHover =
+    'https://www.alairelibre.cl/noticias/site/artic/20210214/imag/foto_0000005020210214164902.jpg';
+
+  imgPathUp = this.imgUpNeutral;
+  currentImgUp = this.imgPathUp;
+  public onUpHover(): void {
+    this.imgPathUp = this.imgUpHover;
+  }
+
+  public onUpLeave(): void {
+    this.imgPathUp = this.currentImgUp;
+  }
+
+  public onUp(): void {}
 }
